054ad9ae18d34bda51faa3cab368aea54b401a5d
Jiho <ruddbsghdus@naver.com>
2025-04-05 16:41:55 +0900

    chore: 초기 세팅

---
+++ src/main/java/aico/backend/AicoApplication.java
@@ -0,0 +1,13 @@
+package aico.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aico/backend/AicoApplicationTests.java
@@ -0,0 +1,13 @@
+package aico.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
