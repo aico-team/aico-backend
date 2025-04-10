473ce95a9afbd6bb92df14ebed74f50bb34f033c
Jiho <ruddbsghdus@naver.com>
2025-04-11 02:28:25 +0900

    feat: 이메일 형식 검증 어노테이션 생성

--- src/main/java/aico/backend/global/annotation/EmailFormatValid.java
+++ src/main/java/aico/backend/global/annotation/EmailFormatValid.java
@@ -1,4 +1,15 @@
 package aico.backend.global.annotation;
 
-public interface EmailFormatValid {
+import aico.backend.global.validator.EmailFormatValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailFormatValidator.class)
+@Documented
+public @interface EmailFormatValid {
+    String message() default "잘못된 이메일 형식입니다.";
 }
