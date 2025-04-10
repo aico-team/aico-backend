9f7419f79684cd12c677236513bfa3f2aaf74ba4
Jiho <ruddbsghdus@naver.com>
2025-04-11 01:36:34 +0900

    feat: 생성자 및 정적 팩토리 메소드 생성

--- src/main/java/aico/backend/user/domain/User.java
+++ src/main/java/aico/backend/user/domain/User.java
@@ -1,9 +1,14 @@
 package aico.backend.user.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,4 +24,16 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    private User(String nickname, String email, String password, Role role) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static User createUser(String nickname, String email, String password, Role role) {
+        return new User(nickname, email, password, role);
+    }
+
 }
