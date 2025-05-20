e569695fba399bc2f0677ebf8b8419e66aca1b5a
Jiho001 <ruddbsghdus@naver.com>
2025-05-20 20:48:59 +0900

    feat: 완료 단계 카운트 함수

---
+++ src/main/java/aico/backend/curriculum/dto/CompletionDto.java
@@ -0,0 +1,4 @@
+package aico.backend.curriculum.dto;
+
+public class CompletionDto {
+}
--- src/main/java/aico/backend/curriculum/repository/CurriRepository.java
+++ src/main/java/aico/backend/curriculum/repository/CurriRepository.java
@@ -3,9 +3,18 @@ package aico.backend.curriculum.repository;
 import aico.backend.curriculum.domain.Curriculum;
 import aico.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CurriRepository extends JpaRepository<Curriculum, Long> {
      List<Curriculum> findByUser(User user);
+
+     @Query(value = """
+                         SELECT COUNT(*)
+                         FROM aico.curriculum, jsonb_each(aico.curriculum.curriculum_map) AS stage
+                         WHERE aico.curriculum.id = :id AND (stage.value ->> 'completed')::boolean = true
+             """, nativeQuery = true)
+     int countCompletedStagesById(@Param("id") Long id);
 }
