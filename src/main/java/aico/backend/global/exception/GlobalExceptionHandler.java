b4b79fde6c0d9972b04ed78343a2f15d1b67f644
Jiho001 <ruddbsghdus@naver.com>
2025-05-25 15:29:19 +0900

    feat: UserNotFoundException 추가

--- src/main/java/aico/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/aico/backend/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package aico.backend.global.exception;
 import aico.backend.global.exception.curriculum.CurriNotFoundException;
 import aico.backend.global.exception.user.ConfirmPasswordMisException;
 import aico.backend.global.exception.user.DuplicatedUserException;
+import aico.backend.global.exception.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +23,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CurriNotFoundException.class)
     public ResponseEntity<?> handleCurriNotFoundException(CurriNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
