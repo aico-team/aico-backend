a4b814389bc0e02298628c958e4f98c35c82a922
Jiho <ruddbsghdus@naver.com>
2025-06-05 01:36:24 +0900

    feat: StudyTime ResponseDto 생성

--- src/main/java/aico/backend/global/security/SecurityConfig.java
+++ src/main/java/aico/backend/global/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173")); // 또는 "*"
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
 
--- src/main/java/aico/backend/studytime/domain/ResponseDto.java
+++ src/main/java/aico/backend/studytime/domain/ResponseDto.java
@@ -1,4 +1,26 @@
 package aico.backend.studytime.domain;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class ResponseDto {
+    LocalDate date;
+    int minutes;
+
+    public ResponseDto(LocalDate date, int minutes) {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public static List<ResponseDto> from(List<StudyTime> records) {
+        List<ResponseDto> response = new ArrayList<>();
+        for(StudyTime record : records) {
+            response.add(new ResponseDto(record.getDate(), record.getStudySeconds()/60));
+        }
+        return response;
+    }
 }
