43adddeccd7242e4917f56110b201779dd04d522
Jiho <ruddbsghdus@naver.com>
2025-06-07 23:44:54 +0900

    feat: 퀴즈 저장 및 오답 조회 dto

--- src/main/java/aico/backend/quiz/QuizSaveRequest.java
+++ src/main/java/aico/backend/quiz/QuizSaveRequest.java
@@ -1,4 +1,19 @@
 package aico.backend.quiz;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class QuizSaveRequest {
+    private String imageUrl;
+    private String fileName;
+    private String originalFileName;
+    private String quiz;
+    private String answer;
 }
+
+
+
--- src/main/java/aico/backend/quiz/WrongAnswerNoteResponse.java
+++ src/main/java/aico/backend/quiz/WrongAnswerNoteResponse.java
@@ -1,4 +1,32 @@
 package aico.backend.quiz;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class WrongAnswerNoteResponse {
+    private String fileName;
+    private String originalFileName;
+    private String imageUrl;
+    private List<QuizItem> quizzes;
+    private LocalDateTime createdAt;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Data
+    public static class QuizItem {
+        private Long id;
+        private String quiz;
+        private String answer;
+        private LocalDateTime createdAt;
+    }
 }
